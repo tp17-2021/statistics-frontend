9ead4b12280e71d7a2b211e6beecd9c62a4225d8
denis116 <denis.klenovic@gmail.com>
2022-04-11 13:53:09 +0200

    EV-161 minify turned off

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,21 @@ let basePath
 
 // if process.env.VITE_BASE_PATH is not undefined, use it as the base path
 if (process.env.VITE_BASE_PATH) {
-  basePath = process.env.VITE_BASE_PATH + '/'
+    basePath = process.env.VITE_BASE_PATH + '/'
 } else {
-  basePath = '/'
+    basePath = '/'
 }
 
 export default defineConfig({
     server: {
         port: 5000,
     },
-    
+
     plugins: [svelte()],
     base: basePath,
     build: {
         // generate sourcemap
-        sourcemap: true
+        sourcemap: true,
+        minify: false,
     }
 });
