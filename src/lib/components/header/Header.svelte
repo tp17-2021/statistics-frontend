15b2b044361deb28d52bdb62c143d86e584e77ea
liborduda <libor.duda@gmail.com>
2022-04-21 15:04:54 +0200

    EV-163 add header stripes, admin page start

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -2,10 +2,22 @@
 import {url} from "@roxi/routify";
 </script>
 
-<style>
+<style lang="scss">
 	header {
 		background-color: black;
 		width: 100%;
+
+		&::after {
+			content: "";
+			display: block;
+			width: 100%;
+			height: 3px;
+			background-image: -webkit-gradient(linear,left top,right top,from(#fff),color-stop(33.3%,#fff),color-stop(33.3%,#003183),color-stop(66.6%,#003183),color-stop(66.6%,#d0190f),to(#d0190f));
+			background-image: -webkit-linear-gradient(left,#fff,#fff 33.3%,#003183 0,#003183 66.6%,#d0190f 0,#d0190f);
+			background-image: linear-gradient(90deg,#fff 0,#fff 33.3%,#003183 0,#003183 66.6%,#d0190f 0,#d0190f);
+			background-size: 150px 100%;
+			background-repeat: repeat;
+		}
 	}
 	.header-content {
 		color: white;
